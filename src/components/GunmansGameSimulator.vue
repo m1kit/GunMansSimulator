beaf715f2aefd6b83c52962f01ddd8e3de39e663
mikit <mikihito0906@gmail.com>
2019-12-09 00:55:17 +0900

    Add players scrollbar

--- src/components/GunmansGameSimulator.vue
+++ src/components/GunmansGameSimulator.vue
@@ -4,21 +4,18 @@
       <h2 class="title">Game Options</h2>
       <div class="columns">
         <b-field label="Number of Turns" class="column">
-          <b-slider v-model="turns" :min="1" :max="10" :step="1" lazy @change="render"></b-slider>
+          <b-numberinput v-model="turns" :min="players" :max="10" size="is-large" :step="1" lazy
+                         @input="render"></b-numberinput>
         </b-field>
-        <!-- b-field label="Number of players">
-          <b-slider v-model="players" :min="1" :max="5" :step="1" lazy disabled></b-slider>
-        </b-field -->
+        <b-field label="Number of players" class="column">
+          <b-numberinput v-model="players" :min="2" :max="Math.min(5, turns)" size="is-large" :step="1" lazy
+                         @input="render"></b-numberinput>
+        </b-field>
       </div>
       <div class="columns">
-        <b-field label="Hit Rate of Player1" class="column">
-          <b-slider v-model="rate[0]" :min="0.00" :max="1.00" :step="0.01" lazy @change="render" type="is-info"></b-slider>
-        </b-field>
-        <b-field label="Hit Rate of Player2" class="column">
-          <b-slider v-model="rate[1]" :min="0.00" :max="1.00" :step="0.01" lazy @change="render" type="is-success"></b-slider>
-        </b-field>
-        <b-field label="Hit Rate of Player3" class="column">
-          <b-slider v-model="rate[2]" :min="0.00" :max="1.00" :step="0.01" lazy @change="render" type="is-danger"></b-slider>
+        <b-field v-for="i in players" v-bind:key="i" :label="'Hit Rate of Player' + i" class="column">
+          <b-slider v-model="rate[i-1]" :min="0.00" :max="1.00" :step="0.01" lazy @change="render"
+                    type="is-info"></b-slider>
         </b-field>
       </div>
       <div class="columns">
@@ -36,15 +33,18 @@
 
 <script>
     import {GameState, GameOption, formatGain} from "../assets/simulator";
-    import {Network} from 'vis-network'
+    import {Network} from 'vis-network';
 
     export default {
         name: "GunmansGameSimulator",
+        components: {
+
+        },
         data() {
             return {
                 players: 3,
                 turns: 3,
-                rate: [0.3, 0.5, 1.0],
+                rate: [0.3, 0.5, 1.0, 1.0, 1.0],
                 status: 'Not yet calculated',
                 options: {
                     layout: {hierarchical: true},
@@ -60,6 +60,7 @@
         },
         methods: {
             render: function () {
+                this.status = "Processing... This may take a while.";
                 const nodes = [];
                 const edges = [];
                 this.simulator = new GameState(this.simulatorOption, 0, Array(this.players).fill(true));
@@ -86,6 +87,7 @@
     margin-top: 32px;
     margin-bottom: 32px;
   }
+
   #graph-container {
     height: 80vh;
     border: solid 2px #9b67df;
